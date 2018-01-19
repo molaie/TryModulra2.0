b7443895bc92b8f3f42e8e1c4d579119ba629406
ahmadmolaie <molaie@gmail.com>
2018-01-19 21:35:28 +0330

    testing options

--- src/Modules/CMSCore/Yooshina.CMSCore.Web/ModuleInitializer.cs
+++ src/Modules/CMSCore/Yooshina.CMSCore.Web/ModuleInitializer.cs
@@ -28,6 +28,36 @@ namespace Yooshina.CMSCore.Web {
 				.AddEntityFrameworkStores<YooshinaDbContext>()
 				.AddDefaultTokenProviders();
 
+
+			services.Configure<IdentityOptions>(options => {
+				// Password settings
+				options.Password.RequireDigit = false;
+				options.Password.RequiredLength = 8;
+				options.Password.RequireNonAlphanumeric = false;
+				options.Password.RequireUppercase = false;
+				options.Password.RequireLowercase = false;
+				options.Password.RequiredUniqueChars = 4;
+
+				// Lockout settings
+				options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+				options.Lockout.MaxFailedAccessAttempts = 10;
+				options.Lockout.AllowedForNewUsers = true;
+				// User settings
+				options.User.RequireUniqueEmail = false;
+			});
+
+			services.ConfigureApplicationCookie(options => {
+				// Cookie settings
+				options.Cookie.HttpOnly = true;
+				options.Cookie.Expiration = TimeSpan.FromHours(1);
+				options.LoginPath = "/Account/Login"; // If the LoginPath is not set here, ASP.NET Core will default to /Account/Login
+				options.LogoutPath = "/Account/Logout"; // If the LogoutPath is not set here, ASP.NET Core will default to /Account/Logout
+				options.AccessDeniedPath = "/Account/AccessDenied"; // If the AccessDeniedPath is not set here, ASP.NET Core will default to /Account/AccessDenied
+				options.SlidingExpiration = true;
+			});
+
+
+
 			container.Configure(_ => {
 				_.For(typeof(IRepository<>)).Use(typeof(Repository<>));
 				_.For<IEmailSender>().Use<AuthMessageSender>();
