0a87e263a68d1662ec4e3223a8e8400443e225ac
ahmadmolaie <molaie@gmail.com>
2018-01-19 17:21:33 +0330

    changed signature

--- src/Core/Yooshina.Modules/ClassLibrary1/IModuleInitializer.cs
+++ src/Core/Yooshina.Modules/ClassLibrary1/IModuleInitializer.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StructureMap;
 
 namespace Yooshina.Core {
+
 	public interface IModuleInitializer {
-		void Init(IServiceCollection serviceCollection);
+		void Init(IServiceCollection serviceCollection, Container container, Assembly asm, IConfiguration config);
 	}
 }
