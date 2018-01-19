95d2d186f3956fcfb55265a80c2932287b836e26
ahmadmolaie <molaie@gmail.com>
2018-01-19 23:40:29 +0330

    added PortalController

---
+++ src/Modules/CMSCore/Yooshina.CMSCore.Web/Controllers/PortalController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Yooshina.CMSCore.Model;
+using Yooshina.Domain;
+
+namespace Yooshina.CMSCore.Web.Controllers {
+
+	[Authorize]
+	public class PortalController : Controller {
+
+
+		private IRepository<Portal> _portalRepo;
+
+
+		public PortalController(IRepository<Portal> portalRepo) {
+			_portalRepo = portalRepo;
+		}
+
+		public ViewResult Index() {
+			return null;
+
+		}
+
+
+	}
+}
