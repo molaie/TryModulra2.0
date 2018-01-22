83922359438236b53ee478eb3075162fcf9ceecc
ahmadmolaie <molaie@gmail.com>
2018-01-22 21:39:41 +0330

    tart of paging

---
+++ src/Components/AspNetCore.MvcPager/PagingInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCore.MvcPager {
+
+	public class PagingInfo {
+		public int TotalItems { get; set; }
+		public int ItemsPerPage { get; set; }
+		public int CurrentPage { get; set; }
+
+
+
+		public int TotalPages =>
+			 (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+
+	}
+}
