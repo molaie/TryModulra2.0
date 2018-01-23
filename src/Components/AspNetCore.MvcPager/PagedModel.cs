b0b84f0c86e8c4983c082bf9e3104786128ad8ea
ahmadmolaie <molaie@gmail.com>
2018-01-24 01:23:57 +0330

    pager changes

---
+++ src/Components/AspNetCore.MvcPager/PagedModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCore.MvcPager {
+	public class PagedModel<T> {
+
+		public PagedModel(IEnumerable<T> _selectedModel, PagingInfo _PagingInfo) {
+			Model = _selectedModel;
+			PagingInfo = _PagingInfo;
+		}
+
+		public IEnumerable<T> Model { get; set; }
+		public PagingInfo PagingInfo { get; set; }
+	}
+}
