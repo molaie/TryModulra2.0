93e71297d9f2b2a2df12d93fb74016d2fe1e0752
ahmadmolaie <molaie@gmail.com>
2018-01-24 20:04:53 +0330

    added new entity

---
+++ src/Modules/CMSCore/Yooshina.CMSCore.Service/Models/Modules.cs
@@ -0,0 +1,11 @@
+﻿using Yooshina.Domain.Models;
+
+namespace Yooshina.CMSCore.Model {
+
+	public class YooshinaModule : LogEntityWithTypedId {
+
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Version { get; set; }
+	}
+}
