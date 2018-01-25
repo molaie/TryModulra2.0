d57c000d7944e4bd89b0d10853b4787b7ad87a79
ahmadmolaie <molaie@gmail.com>
2018-01-25 18:44:38 +0330

    added ViewLayoutAttribute

---
+++ Yooshina.Web.Common/ViewLayoutAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Yooshina.Web.Common {
+
+	public class ViewLayoutAttribute : ResultFilterAttribute {
+		private string layout;
+		public ViewLayoutAttribute(string layout) {
+			this.layout = layout;
+		}
+
+		public override void OnResultExecuting(ResultExecutingContext context) {
+			var viewResult = context.Result as ViewResult;
+			if (viewResult != null) {
+				viewResult.ViewData["Layout"] = this.layout;
+			}
+		}
+	}
+}
