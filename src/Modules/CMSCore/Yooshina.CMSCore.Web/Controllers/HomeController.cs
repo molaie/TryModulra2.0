9d0b06ff29ee188dbe5dcc43eb093affdaf1ad93
ahmadmolaie <molaie@gmail.com>
2018-01-25 19:55:05 +0330

    cleanup

--- src/Modules/CMSCore/Yooshina.CMSCore.Web/Controllers/HomeController.cs
+++ src/Modules/CMSCore/Yooshina.CMSCore.Web/Controllers/HomeController.cs
@@ -3,40 +3,12 @@
 namespace Yooshina.CMSCore.Web.Controllers {
 	public class HomeController : Controller {
 
-		//private readonly IContainer _Container;
-
-
-		public HomeController(
-			//IContainer _c2
-			) {
-			//_Container = _c2;
+		public HomeController() {
 		}
 
 		public IActionResult Index() {
 			return View();
 		}
-
-		public IActionResult About() {
-			ViewData["Message"] = "Your application description page.";
-
-			return View();
-		}
-
-		public IActionResult Contact() {
-			ViewData["Message"] = "Your contact page.";
-
-			return View();
-		}
-
-
-		public IActionResult SM() {
-			//var txt = _Container.WhatDoIHave(typeName: "Sample").Replace("\\r\\n", "<br />");
-			//txt = _Container.WhatDoIHave().Replace("\\r\\n", "<br />");
-			//ViewData["Message"] = txt;
-			return View();
-		}
-
-
 
 		public IActionResult Error() {
 			return View();
