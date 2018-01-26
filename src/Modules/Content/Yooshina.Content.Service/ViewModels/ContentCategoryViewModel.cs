c195ba3429bd5f20e68d409cb735ce0ef79374f6
ahmadmolaie <molaie@gmail.com>
2018-01-26 16:49:40 +0330

    added ContentCategoryViewModel

---
+++ src/Modules/Content/Yooshina.Content.Service/ViewModels/ContentCategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Yooshina.Content.ViewModels {
+
+	public class ContentCategoryViewModel {
+
+		public long Id { get; set; }
+		public string Title { get; set; }
+		public long? ParentId { get; set; }
+
+	}
+}
